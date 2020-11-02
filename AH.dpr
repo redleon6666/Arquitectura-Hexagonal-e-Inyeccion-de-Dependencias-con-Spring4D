fc4100f150d4eaf1da89cad5749032365eaa3d1f
redleon6666 <72522897+redleon6666@users.noreply.github.com>
2020-11-02 20:03:32 +0100

    Refactory

--- AH.dpr
+++ AH.dpr
@@ -3,7 +3,7 @@ program AH;
 uses
   Vcl.Forms,
   uFormProxy in 'Source\Infrastructure\Configuration\Proxy\uFormProxy.pas' {FormProxy},
-  uRegistrationConfiguration in 'Source\DependencyInjection\uRegistrationConfiguration.pas',
+  uRegistrationConfiguration in 'Source\Infrastructure\DependencyInjection\uRegistrationConfiguration.pas',
   uProxy in 'Source\Domain\Configuration\Proxy\Entity\uProxy.pas',
   uProxyDTO in 'Source\Application\Configuration\Proxy\Entity\uProxyDTO.pas',
   uIProxyGetter in 'Source\Application\Configuration\Proxy\Getter\uIProxyGetter.pas',
